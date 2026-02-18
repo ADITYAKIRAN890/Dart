b168d9264b566d5303b1717913064b7af6a539df
Aditya Kiran Shukla <adityashukla600700@gmail.com>
2026-02-18 14:07:04 +0530

    new changes

--- 2.dart
+++ 2.dart
@@ -1,6 +1,15 @@
 void main(){
-  var a = 10;
-  var b = 20;
-  var c = a + b;
-  print(c);
+  // var a = 10;
+  // var b = 20;
+  // var c = a + b;
+  // print(c);
+
+  Object a = "Hello";
+  print(a);
+  //print(a.length);// gives error because a is of type Object and Object does not have length property
+
+  dynamic b = "Hello";
+  print(b);
+  print(b.length); // works because b is of type dynamic and dynamic allows any type of value and any property or method can be accessed on it without compile-time checks
+
 }
